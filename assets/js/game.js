624997fdc142e00edb96471260d6721969b43018
Lisa <lisacjheinhold@gmail.com>
2022-04-25 17:25:11 -0600

    add more enemies to fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,10 +3,10 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
-var fight = function () {
+var fight = function (enemyName) {
     //alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -45,4 +45,6 @@ var fight = function () {
         window.alert("You need to choose a valid option. Try again!")
     }
 };
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
