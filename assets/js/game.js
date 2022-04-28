a607cca4b880b7c56797435bbc21d86a80f76a90
Lisa <lisacjheinhold@gmail.com>
2022-04-28 13:39:26 -0600

    fix player name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -114,8 +114,17 @@ var shop = function() {
             var value = Math.floor(Math.random()*(max - min + 1) + min);
             return value;
         };
+    //function to set name
+    var getPlayerName = function() {
+        var name = "";
+        while (name === "" || name === null) {
+            name = prompt("What is your robot's name?");
+        }
+        console.log("Your robot's name is " + name);
+        return name;
+    };
     var playerInfo = {
-        name: window.prompt("What is your robot's name?"),
+        name: getPlayerName(),
         health: 100,
         attack: 10,
         money: 10,
